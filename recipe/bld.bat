5fdd4b16073aac82e2609844e8e5db192cdf15fc
Chris Meyer <cmeyer1969@gmail.com>
2019-01-07 23:26:35 -0800

    Initial feedstock commit with conda-smithy 3.2.3.dev37.

---
+++ recipe/bld.bat
@@ -0,0 +1,2 @@
+%PYTHON% -m pip install --no-deps --ignore-installed https://github.com/nion-software/nionui-tool/releases/download/%PKG_VERSION%/nionui_tool-%PKG_VERSION%-cp36.cp37-none-win_amd64.whl
+if errorlevel 1 exit 1
