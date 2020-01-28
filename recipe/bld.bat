52546a756b0b10c2f2ee1c40aa31e8e140d9a51b
Chris Meyer <cmeyer1969@gmail.com>
2020-01-28 10:06:06 -0800

    Update recipe for Python 3.8.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
-%PYTHON% -m pip install --no-deps --ignore-installed https://github.com/nion-software/nionui-tool/releases/download/%PKG_VERSION%/nionui_tool-%PKG_VERSION%-cp36.cp37-none-win_amd64.whl
+%PYTHON% -m pip install --no-deps --ignore-installed https://github.com/nion-software/nionui-tool/releases/download/%PKG_VERSION%/nionui_tool-%PKG_VERSION%-cp36.cp37.cp38-none-win_amd64.whl
 if errorlevel 1 exit 1
