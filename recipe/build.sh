47988b0278d8c3b82c61b48312ffd99f95237aef
Chris Meyer <cmeyer1969@gmail.com>
2020-09-25 09:20:47 -0700

    Use CMake to build conda version.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
-# install using pip from the whl files on PyPI
-
-if [ `uname` == Darwin ]; then
-    WHL_FILE=https://github.com/nion-software/nionui-tool/releases/download/${PKG_VERSION}/nionui_tool-${PKG_VERSION}-cp36.cp37.cp38-abi3-macosx_10_11_intel.whl
+if [[ "$target_platform" == linux* ]]; then
+  # set
+  # pwd
+  # ls -R .
+  # cp ${RECIPE_DIR}/CMakeLists.txt launcher/CMakeLists.txt
+  pushd launcher
+  # conda info
+  # conda list
+  # conda env list
+  # "${PYTHON}" --version
+  # default python is from the activate conda environment which is not the build environment; specify python to cmake
+  cmake CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON"
+  cmake --build . --config Release
+  # ls -R .
+  # readelf -d build/NionUILauncher
+  # readelf -dr build/NionUILauncher | grep QAbstractItem
+  mkdir -p linux
+  mv build linux/x64
+  popd
+  "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
 
-if [ `uname` == Linux ]; then
-    WHL_FILE=https://github.com/nion-software/nionui-tool/releases/download/${PKG_VERSION}/nionui_tool-${PKG_VERSION}-cp36.cp37.cp38-abi3-manylinux1_x86_64.whl
+if [[ "$target_platform" == "osx-64" ]]; then
+  # set
+  # pwd
+  # ls -R .
+  # cp ${RECIPE_DIR}/CMakeLists.txt launcher/CMakeLists.txt
+  pushd launcher
+  # conda info
+  # conda list
+  # conda env list
+  # "${PYTHON}" --version
+  # default python is from the activate conda environment which is not the build environment; specify python to cmake
+  cmake CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON"
+  cmake --build . --config Release
+  # ls -R .
+  mkdir -p build/Release
+  mv build/*.app build/Release
+  popd
+  # ls -R .
+  "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
-
-pip install --no-deps --ignore-installed $WHL_FILE
